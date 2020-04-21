f075cc34010b664c4579736df26247a8ef3041e5
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-21 18:08:51 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import uuid
+import os
+from flask import Flask, request, jsonify
+from pathlib import Path
+
+def ensure_dir(dirname):
+    dirname = Path(dirname)
+    if not dirname.is_dir():
+        dirname.mkdir(parents=True, exist_ok=False)
+
+app = Flask(__name__, static_folder='assets')
+
+@app.route('/', methods=['GET', 'POST', 'PUT'])
+def upload():
+    print(request.get_json())
+    file = request.files['file']
+    filename = str(uuid.uuid1())
+    file.save(os.path.join('assets', filename))
+    return f'https://plapp-resource-service.herokuapp.com/assets/{filename}'
+
+@app.route('/assets/<path:path>', methods=['GET'])
+def serve(path):
+    return app.send_static_file(path)
+
+if __name__ == '__main__':
+    ensure_dir('assets')
+    app.run()
